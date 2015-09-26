faa08c6075ff3d53faaf47e72e817374c3c16ff0
MWisBest <repinski23@gmail.com>
2015-09-26 12:25:59 -0500

    Major legibility improvement for hashing functions.

--- VLTEdit/HashUtil.cs
+++ VLTEdit/HashUtil.cs
@@ -9,227 +9,186 @@ namespace VLTEdit
 	 */
 	public class HashUtil // OBF: o.cs
 	{
-		public static uint getHash32( string toHash, uint magic = 2882400000u ) // 0xABCDEF00
+		private static void mix32( ref uint a, ref uint b, ref uint c )
+		{
+			a = c >> 13 ^ ( a - b - c );
+			b = a << 8 ^ ( b - c - a );
+			c = b >> 13 ^ ( c - a - b );
+			a = c >> 12 ^ ( a - b - c );
+			b = a << 16 ^ ( b - c - a );
+			c = b >> 5 ^ ( c - a - b );
+			a = c >> 3 ^ ( a - b - c );
+			b = a << 10 ^ ( b - c - a );
+			c = b >> 15 ^ ( c - a - b );
+		}
+
+		public static uint getHash32( string toHash, uint magic = 0xABCDEF00u )
 		{
 			int num = 0;
 			int i = toHash.Length;
-			uint num2 = 2654435769u; // 0x9E3779B9
-			uint num3 = num2;
-			uint num4 = magic;
+			uint a = 0x9E3779B9u, b = 0x9E3779B9u, c = magic;
 			while( i >= 12 )
 			{
-				num2 += (uint)( toHash[num] + ( (uint)toHash[1 + num] << 8 ) + ( (uint)toHash[2 + num] << 16 ) + ( (uint)toHash[3 + num] << 24 ) );
-				num3 += (uint)( toHash[4 + num] + ( (uint)toHash[5 + num] << 8 ) + ( (uint)toHash[6 + num] << 16 ) + ( (uint)toHash[7 + num] << 24 ) );
-				num4 += (uint)( toHash[8 + num] + ( (uint)toHash[9 + num] << 8 ) + ( (uint)toHash[10 + num] << 16 ) + ( (uint)toHash[11 + num] << 24 ) );
-				num2 = num4 >> 13 ^ ( num2 - num3 - num4 );
-				num3 = num2 << 8 ^ ( num3 - num4 - num2 );
-				num4 = num3 >> 13 ^ ( num4 - num2 - num3 );
-				num2 = num4 >> 12 ^ ( num2 - num3 - num4 );
-				num3 = num2 << 16 ^ ( num3 - num4 - num2 );
-				num4 = num3 >> 5 ^ ( num4 - num2 - num3 );
-				num2 = num4 >> 3 ^ ( num2 - num3 - num4 );
-				num3 = num2 << 10 ^ ( num3 - num4 - num2 );
-				num4 = num3 >> 15 ^ ( num4 - num2 - num3 );
+				a += (uint)( toHash[num] + ( (uint)toHash[1 + num] << 8 ) + ( (uint)toHash[2 + num] << 16 ) + ( (uint)toHash[3 + num] << 24 ) );
+				b += (uint)( toHash[4 + num] + ( (uint)toHash[5 + num] << 8 ) + ( (uint)toHash[6 + num] << 16 ) + ( (uint)toHash[7 + num] << 24 ) );
+				c += (uint)( toHash[8 + num] + ( (uint)toHash[9 + num] << 8 ) + ( (uint)toHash[10 + num] << 16 ) + ( (uint)toHash[11 + num] << 24 ) );
+				mix32( ref a, ref b, ref c );
 				num += 12;
 				i -= 12;
 			}
-			num4 += (uint)toHash.Length;
+			c += (uint)toHash.Length;
 			switch( i )
 			{
+				// NOTE: C# doesn't allow fallthroughs, so as a workaround we use goto's instead.
 				case 11:
-					num4 += (uint)( (uint)toHash[10 + num] << 24 );
+					c += (uint)toHash[10 + num] << 24;
 					goto TEN;
-				case 10:
-				TEN:
-					num4 += (uint)( (uint)toHash[9 + num] << 16 );
+				case 10: TEN:
+					c += (uint)toHash[9 + num] << 16;
 					goto NINE;
-				case 9:
-				NINE:
-					num4 += (uint)( (uint)toHash[8 + num] << 8 );
+				case 9:  NINE:
+					c += (uint)toHash[8 + num] << 8;
 					goto EIGHT;
-				case 8:
-				EIGHT:
-					num3 += (uint)( (uint)toHash[7 + num] << 24 );
+				case 8:  EIGHT:
+					b += (uint)toHash[7 + num] << 24;
 					goto SEVEN;
-				case 7:
-				SEVEN:
-					num3 += (uint)( (uint)toHash[6 + num] << 16 );
+				case 7:  SEVEN:
+					b += (uint)toHash[6 + num] << 16;
 					goto SIX;
-				case 6:
-				SIX:
-					num3 += (uint)( (uint)toHash[5 + num] << 8 );
+				case 6:  SIX:
+					b += (uint)toHash[5 + num] << 8;
 					goto FIVE;
-				case 5:
-				FIVE:
-					num3 += (uint)toHash[4 + num];
+				case 5:  FIVE:
+					b += (uint)toHash[4 + num];
 					goto FOUR;
-				case 4:
-				FOUR:
-					num2 += (uint)( (uint)toHash[3 + num] << 24 );
+				case 4:  FOUR:
+					a += (uint)toHash[3 + num] << 24;
 					goto THREE;
-				case 3:
-				THREE:
-					num2 += (uint)( (uint)toHash[2 + num] << 16 );
+				case 3:  THREE:
+					a += (uint)toHash[2 + num] << 16;
 					goto TWO;
-				case 2:
-				TWO:
-					num2 += (uint)( (uint)toHash[1 + num] << 8 );
+				case 2:  TWO:
+					a += (uint)toHash[1 + num] << 8;
 					goto ONE;
-				case 1:
-				ONE:
-					num2 += (uint)toHash[num];
+				case 1:  ONE:
+					a += (uint)toHash[num];
 					break;
 				default:
 					break;
 			}
-			num2 = num4 >> 13 ^ ( num2 - num3 - num4 );
-			num3 = num2 << 8 ^ ( num3 - num4 - num2 );
-			num4 = num3 >> 13 ^ ( num4 - num2 - num3 );
-			num2 = num4 >> 12 ^ ( num2 - num3 - num4 );
-			num3 = num2 << 16 ^ ( num3 - num4 - num2 );
-			num4 = num3 >> 5 ^ ( num4 - num2 - num3 );
-			num2 = num4 >> 3 ^ ( num2 - num3 - num4 );
-			num3 = num2 << 10 ^ ( num3 - num4 - num2 );
-			return ( num3 >> 15 ^ ( num4 - num2 - num3 ) );
+			mix32( ref a, ref b, ref c );
+			return c;
 		}
 
-		public static ulong getHash64( string toHash, ulong magic = 1234605617886129920uL ) // 0x11223344ABCDEF00
+		private static void mix64( ref ulong a, ref ulong b, ref ulong c )
+		{
+			a = c >> 43 ^ ( a - b - c );
+			b = a << 9 ^ ( b - c - a );
+			c = b >> 8 ^ ( c - a - b );
+			a = c >> 38 ^ ( a - b - c );
+			b = a << 23 ^ ( b - c - a );
+			c = b >> 5 ^ ( c - a - b );
+			a = c >> 35 ^ ( a - b - c );
+			b = a << 49 ^ ( b - c - a );
+			c = b >> 11 ^ ( c - a - b );
+			a = c >> 12 ^ ( a - b - c );
+			b = a << 18 ^ ( b - c - a );
+			c = b >> 22 ^ ( c - a - b );
+		}
+
+		public static ulong getHash64( string toHash, ulong magic = 0x11223344ABCDEF00uL )
 		{
 			int num = 0;
 			int i = toHash.Length;
-			ulong num2 = 11400714819323198483uL; // 0x9E3779B97F4A7C13
-			ulong num3 = num2;
-			ulong num4 = magic;
+			ulong a = 0x9E3779B97F4A7C13uL, b = 0x9E3779B97F4A7C13uL, c = magic;
 			byte[] bytes = Encoding.ASCII.GetBytes( toHash );
 			while( i >= 24 )
 			{
-				num2 += BitConverter.ToUInt64( bytes, num );
-				num3 += BitConverter.ToUInt64( bytes, num + 8 );
-				num4 += BitConverter.ToUInt64( bytes, num + 16 );
-				num2 = num4 >> 43 ^ ( num2 - num3 - num4 );
-				num3 = num2 << 9 ^ ( num3 - num4 - num2 );
-				num4 = num3 >> 8 ^ ( num4 - num2 - num3 );
-				num2 = num4 >> 38 ^ ( num2 - num3 - num4 );
-				num3 = num2 << 23 ^ ( num3 - num4 - num2 );
-				num4 = num3 >> 5 ^ ( num4 - num2 - num3 );
-				num2 = num4 >> 35 ^ ( num2 - num3 - num4 );
-				num3 = num2 << 49 ^ ( num3 - num4 - num2 );
-				num4 = num3 >> 11 ^ ( num4 - num2 - num3 );
-				num2 = num4 >> 12 ^ ( num2 - num3 - num4 );
-				num3 = num2 << 18 ^ ( num3 - num4 - num2 );
-				num4 = num3 >> 22 ^ ( num4 - num2 - num3 );
-				i -= 24;
+				a += BitConverter.ToUInt64( bytes, num );
+				b += BitConverter.ToUInt64( bytes, num + 8 );
+				c += BitConverter.ToUInt64( bytes, num + 16 );
+				mix64( ref a, ref b, ref c );
 				num += 24;
+				i -= 24;
 			}
-			num4 += (ulong)( (long)toHash.Length );
+			c += (ulong)toHash.Length;
 			switch( i )
 			{
+				// NOTE: C# doesn't allow fallthroughs, so as a workaround we use goto's instead.
 				case 23:
-					num4 += (ulong)toHash[22] << 56;
+					c += (ulong)toHash[22] << 56;
 					goto TWENTYTWO;
-				case 22:
-				TWENTYTWO:
-					num4 += (ulong)toHash[21] << 48;
+				case 22: TWENTYTWO:
+					c += (ulong)toHash[21] << 48;
 					goto TWENTYONE;
-				case 21:
-				TWENTYONE:
-					num4 += (ulong)toHash[20] << 40;
+				case 21: TWENTYONE:
+					c += (ulong)toHash[20] << 40;
 					goto TWENTY;
-				case 20:
-				TWENTY:
-					num4 += (ulong)toHash[19] << 32;
+				case 20: TWENTY:
+					c += (ulong)toHash[19] << 32;
 					goto NINETEEN;
-				case 19:
-				NINETEEN:
-					num4 += (ulong)toHash[18] << 24;
+				case 19: NINETEEN:
+					c += (ulong)toHash[18] << 24;
 					goto EIGHTEEN;
-				case 18:
-				EIGHTEEN:
-					num4 += (ulong)toHash[17] << 16;
+				case 18: EIGHTEEN:
+					c += (ulong)toHash[17] << 16;
 					goto SEVENTEEN;
-				case 17:
-				SEVENTEEN:
-					num4 += (ulong)toHash[16] << 8;
+				case 17: SEVENTEEN:
+					c += (ulong)toHash[16] << 8;
 					goto SIXTEEN;
-				case 16:
-				SIXTEEN:
-					num3 += (ulong)toHash[15] << 56;
+				case 16: SIXTEEN:
+					b += (ulong)toHash[15] << 56;
 					goto FIFTEEN;
-				case 15:
-				FIFTEEN:
-					num3 += (ulong)toHash[14] << 48;
+				case 15: FIFTEEN:
+					b += (ulong)toHash[14] << 48;
 					goto FOURTEEN;
-				case 14:
-				FOURTEEN:
-					num3 += (ulong)toHash[13] << 40;
+				case 14: FOURTEEN:
+					b += (ulong)toHash[13] << 40;
 					goto THIRTEEN;
-				case 13:
-				THIRTEEN:
-					num3 += (ulong)toHash[12] << 32;
+				case 13: THIRTEEN:
+					b += (ulong)toHash[12] << 32;
 					goto TWELVE;
-				case 12:
-				TWELVE:
-					num3 += (ulong)toHash[11] << 24;
+				case 12: TWELVE:
+					b += (ulong)toHash[11] << 24;
 					goto ELEVEN;
-				case 11:
-				ELEVEN:
-					num3 += (ulong)toHash[10] << 16;
+				case 11: ELEVEN:
+					b += (ulong)toHash[10] << 16;
 					goto TEN;
-				case 10:
-				TEN:
-					num3 += (ulong)toHash[9] << 8;
+				case 10: TEN:
+					b += (ulong)toHash[9] << 8;
 					goto NINE;
-				case 9:
-				NINE:
-					num3 += (ulong)toHash[8];
+				case 9:  NINE:
+					b += (ulong)toHash[8];
 					goto EIGHT;
-				case 8:
-				EIGHT:
-					num2 += (ulong)toHash[7] << 56;
+				case 8:  EIGHT:
+					a += (ulong)toHash[7] << 56;
 					goto SEVEN;
-				case 7:
-				SEVEN:
-					num2 += (ulong)toHash[6] << 48;
+				case 7:  SEVEN:
+					a += (ulong)toHash[6] << 48;
 					goto SIX;
-				case 6:
-				SIX:
-					num2 += (ulong)toHash[5] << 40;
+				case 6:  SIX:
+					a += (ulong)toHash[5] << 40;
 					goto FIVE;
-				case 5:
-				FIVE:
-					num2 += (ulong)toHash[4] << 32;
+				case 5:  FIVE:
+					a += (ulong)toHash[4] << 32;
 					goto FOUR;
-				case 4:
-				FOUR:
-					num2 += (ulong)toHash[3] << 24;
+				case 4:  FOUR:
+					a += (ulong)toHash[3] << 24;
 					goto THREE;
-				case 3:
-				THREE:
-					num2 += (ulong)toHash[2] << 16;
+				case 3:  THREE:
+					a += (ulong)toHash[2] << 16;
 					goto TWO;
-				case 2:
-				TWO:
-					num2 += (ulong)toHash[1] << 8;
+				case 2:  TWO:
+					a += (ulong)toHash[1] << 8;
 					goto ONE;
-				case 1:
-				ONE:
-					num2 += (ulong)toHash[0];
+				case 1:  ONE:
+					a += (ulong)toHash[0];
 					break;
 				default:
 					break;
 			}
-			num2 = num4 >> 43 ^ ( num2 - num3 - num4 );
-			num3 = num2 << 9 ^ ( num3 - num4 - num2 );
-			num4 = num3 >> 8 ^ ( num4 - num2 - num3 );
-			num2 = num4 >> 38 ^ ( num2 - num3 - num4 );
-			num3 = num2 << 23 ^ ( num3 - num4 - num2 );
-			num4 = num3 >> 5 ^ ( num4 - num2 - num3 );
-			num2 = num4 >> 35 ^ ( num2 - num3 - num4 );
-			num3 = num2 << 49 ^ ( num3 - num4 - num2 );
-			num4 = num3 >> 11 ^ ( num4 - num2 - num3 );
-			num2 = num4 >> 12 ^ ( num2 - num3 - num4 );
-			num3 = num2 << 18 ^ ( num3 - num4 - num2 );
-			return ( num3 >> 22 ^ ( num4 - num2 - num3 ) );
+			mix64( ref a, ref b, ref c );
+			return c;
 		}
 	}
 }

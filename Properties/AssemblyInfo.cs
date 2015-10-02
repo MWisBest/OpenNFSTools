0b37661cc17b1b8719418174db5b1f05f3d0e2c2
MWisBest <repinski23@gmail.com>
2015-10-02 16:11:25 -0500

    VLTEdit: Build versioning overhaul.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using static VLTEdit.BuildConfig;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -29,4 +30,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion( "1.1.*" )]
+[assembly: AssemblyVersion( MAJOR_VER + "." + MINOR_VER + ".*" )]
+[assembly: AssemblyInformationalVersion( MAJOR_VER + "." + MINOR_VER + "." + PATCH_VER )]
--- VLTEdit/BuildConfig.cs
+++ VLTEdit/BuildConfig.cs
@@ -4,5 +4,8 @@ namespace VLTEdit
 	{
 		public static bool DEBUG = false;
 		public static bool CARBON = false;
+		public const string MAJOR_VER = "1";
+		public const string MINOR_VER = "1";
+		public const string PATCH_VER = "0";
 	}
 }

082c658606d303257e4fd1e4e554786ba065f282
MWisBest <repinski23@gmail.com>
2015-12-23 23:47:14 -0600

    VLTEdit: v1.2.0

--- VLTEdit/BuildConfig.cs
+++ VLTEdit/BuildConfig.cs
@@ -5,7 +5,7 @@ namespace VLTEdit
 		public static bool DEBUG = false;
 		public static bool CARBON = false;
 		public const string MAJOR_VER = "1";
-		public const string MINOR_VER = "1";
+		public const string MINOR_VER = "2";
 		public const string PATCH_VER = "0";
 
 		/**

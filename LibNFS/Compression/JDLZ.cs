658543ad5209e70046b65cc8ebc7bff6e2664dd2
Kyle Repinski <repinski23@gmail.com>
2016-01-17 17:43:24 -0600

    LibNFS: Add JDLZ compressor, courtesy of 'zombie28'

--- LibNFS/Compression/JDLZ.cs
+++ LibNFS/Compression/JDLZ.cs
@@ -82,6 +82,181 @@ namespace NFSTools.LibNFS.Compression
 			return output;
 		}
 
+		/// <summary>
+		/// This lovely JDLZ compressor was written by the user "zombie28" of the encode.ru forums.
+		/// Its compression ratios are within a few percent of the NFS games' JDLZ compressor. Awesome!!
+		/// </summary>
+		/// <param name="input">bytes to compress with JDLZ</param>
+		/// <returns>JDLZ-compressed bytes, w/ 16 byte header</returns>
+		public static byte[] compress( byte[] input )
+		{
+			if( input == null )
+			{
+				return null;
+			}
+
+			const int HeaderSize = 16;
+			const int MinMatchLength = 3;
+			const int MaxSearchDepth = 16;
+
+			int inputBytes = input.Length;
+			byte[] output = new byte[inputBytes + ( ( inputBytes + 7 ) / 8 ) + HeaderSize + 1];
+			int[] hashPos = new int[0x2000];
+			int[] hashChain = new int[inputBytes];
+
+			int outPos = 0;
+			int inPos = 0;
+			byte flags1bit = 1;
+			byte flags2bit = 1;
+			byte flags1 = 0;
+			byte flags2 = 0;
+
+			output[outPos++] = 0x4A; // 'J'
+			output[outPos++] = 0x44; // 'D'
+			output[outPos++] = 0x4C; // 'L'
+			output[outPos++] = 0x5A; // 'Z'
+			output[outPos++] = 0x02;
+			output[outPos++] = 0x10;
+			output[outPos++] = 0x00;
+			output[outPos++] = 0x00;
+			output[outPos++] = (byte)inputBytes;
+			output[outPos++] = (byte)( inputBytes >> 8 );
+			output[outPos++] = (byte)( inputBytes >> 16 );
+			output[outPos++] = (byte)( inputBytes >> 24 );
+			outPos += 4;
+
+			int flags1Pos = outPos++;
+			int flags2Pos = outPos++;
+
+			flags1bit <<= 1;
+			output[outPos++] = input[inPos++];
+			inputBytes--;
+
+			while( inputBytes > 0 )
+			{
+				int bestMatchLength = MinMatchLength - 1;
+				int bestMatchDist = 0;
+
+				if( inputBytes >= MinMatchLength )
+				{
+					int hash = ( -0x1A1 * ( input[inPos] ^ ( ( input[inPos + 1] ^ ( input[inPos + 2] << 4 ) ) << 4 ) ) ) & 0x1FFF;
+					int matchPos = hashPos[hash];
+					hashPos[hash] = inPos;
+					hashChain[inPos] = matchPos;
+					int prevMatchPos = inPos;
+
+					for( int i = 0; i < MaxSearchDepth; i++ )
+					{
+						int matchDist = inPos - matchPos;
+
+						if( matchDist > 2064 || matchPos >= prevMatchPos )
+						{
+							break;
+						}
+
+						int matchLengthLimit = matchDist <= 16 ? 4098 : 34;
+						int maxMatchLength = inputBytes;
+
+						if( maxMatchLength > matchLengthLimit )
+						{
+							maxMatchLength = matchLengthLimit;
+						}
+						if( bestMatchLength >= maxMatchLength )
+						{
+							break;
+						}
+
+						int matchLength = 0;
+						while( ( matchLength < maxMatchLength ) && ( input[inPos + matchLength] == input[matchPos + matchLength] ) )
+						{
+							matchLength++;
+						}
+
+						if( matchLength > bestMatchLength )
+						{
+							bestMatchLength = matchLength;
+							bestMatchDist = matchDist;
+						}
+
+						prevMatchPos = matchPos;
+						matchPos = hashChain[matchPos];
+					}
+				}
+
+				if( bestMatchLength >= MinMatchLength )
+				{
+					flags1 |= flags1bit;
+					inPos += bestMatchLength;
+					inputBytes -= bestMatchLength;
+					bestMatchLength -= MinMatchLength;
+
+					if( bestMatchDist < 17 )
+					{
+						flags2 |= flags2bit;
+						output[outPos++] = (byte)( ( bestMatchDist - 1 ) | ( ( bestMatchLength >> 4 ) & 0xF0 ) );
+						output[outPos++] = (byte)bestMatchLength;
+					}
+					else
+					{
+						bestMatchDist -= 17;
+						output[outPos++] = (byte)( bestMatchLength | ( ( bestMatchDist >> 3 ) & 0xE0 ) );
+						output[outPos++] = (byte)bestMatchDist;
+					}
+
+					flags2bit <<= 1;
+				}
+				else
+				{
+					output[outPos++] = input[inPos++];
+					inputBytes--;
+				}
+
+				flags1bit <<= 1;
+
+				if( flags1bit == 0 )
+				{
+					output[flags1Pos] = flags1;
+					flags1 = 0;
+					flags1Pos = outPos++;
+					flags1bit = 1;
+				}
+
+				if( flags2bit == 0 )
+				{
+					output[flags2Pos] = flags2;
+					flags2 = 0;
+					flags2Pos = outPos++;
+					flags2bit = 1;
+				}
+			}
+
+			if( flags2bit > 1 )
+			{
+				output[flags2Pos] = flags2;
+			}
+			else if( flags2Pos == outPos - 1 )
+			{
+				outPos = flags2Pos;
+			}
+
+			if( flags1bit > 1 )
+			{
+				output[flags1Pos] = flags1;
+			}
+			else if( flags1Pos == outPos - 1 )
+			{
+				outPos = flags1Pos;
+			}
+
+			output[12] = (byte)outPos;
+			output[13] = (byte)( outPos >> 8 );
+			output[14] = (byte)( outPos >> 16 );
+			output[15] = (byte)( outPos >> 24 );
+
+			Array.Resize( ref output, outPos );
+			return output;
+		}
+
 		// TODO
 		// All the other tools I've found literally copy and pasted the game's assembly code for the JDLZ compressor.
 		// What a joke! No wonder they all packed their binaries and didn't release source code... their stuff is garbage!

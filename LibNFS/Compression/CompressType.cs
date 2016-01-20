6fd76dd39e82aaa19ffda85fbf8b744ff41e80d0
Kyle Repinski <repinski23@gmail.com>
2016-01-20 17:52:22 -0600

    LibNFS: Compression: Add automatic utilities.
    TODO: Docs.

---
+++ LibNFS/Compression/CompressType.cs
@@ -0,0 +1,12 @@
+﻿namespace NFSTools.LibNFS.Compression
+{
+	public enum CompressType : uint
+	{
+		NULL =  0,
+		RAWW =  1 << 0,
+		JDLZ =  1 << 1,
+		HUFF =  1 << 2,
+		RFPK =  1 << 3,
+		ALL  = (1 << 30) - 1
+	}
+}
---
+++ LibNFS/Compression/CompressUtils.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace NFSTools.LibNFS.Compression
+{
+	public static class CompressUtils
+	{
+		public static byte[] decompress_auto( byte[] input, out CompressType usedType )
+		{
+			// Sanity checking...
+			if( input == null )
+			{
+				throw new ArgumentNullException( "input" );
+			}
+
+			byte[] output = null;
+			usedType = CompressType.NULL;
+
+			try
+			{
+				output = RAWW.decompress( input );
+				usedType = CompressType.RAWW;
+			}
+			catch( InvalidDataException )
+			{
+				try
+				{
+					output = JDLZ.decompress( input );
+					usedType = CompressType.JDLZ;
+				}
+				catch( InvalidDataException )
+				{
+					// Try more types as added...
+				}
+			}
+
+			return output;
+		}
+
+		public static byte[] compress_auto( byte[] input, out CompressType usedType, CompressType allowedTypes = CompressType.ALL )
+		{
+			// Sanity checking...
+			if( input == null )
+			{
+				throw new ArgumentNullException( "input" );
+			}
+			if( allowedTypes == CompressType.NULL )
+			{
+				throw new ArgumentException( "Allowed compression type cannot be NULL!", "allowedTypes" );
+			}
+
+			Dictionary<CompressType, byte[]> mapping = new Dictionary<CompressType, byte[]>();
+			byte[] output = null;
+			usedType = CompressType.NULL;
+
+			if( ( allowedTypes & CompressType.RAWW ) == CompressType.RAWW )
+			{
+				mapping.Add( CompressType.RAWW, RAWW.compress( input ) );
+			}
+			if( ( allowedTypes & CompressType.JDLZ ) == CompressType.JDLZ )
+			{
+				mapping.Add( CompressType.JDLZ, JDLZ.compress( input ) );
+			}
+
+			int minLength = int.MaxValue;
+
+			foreach( KeyValuePair<CompressType, byte[]> entry in mapping )
+			{
+				// NOTE: if the length is EQUAL TO the shortest one, we decide based on the complexity of that type's decompression.
+				if( ( entry.Value.Length < minLength ) || ( entry.Value.Length == minLength && entry.Key < usedType ) )
+				{
+					minLength = entry.Value.Length;
+					output = entry.Value;
+					usedType = entry.Key;
+				}
+			}
+
+			return output;
+		}
+	}
+}
